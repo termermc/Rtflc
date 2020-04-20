318ace0383f7c56a8a8e74ce1152d78258e687d3
= <termer@protonmail.com>
2020-04-20 10:55:14 -0400

    delete old source files

--- src/net/termer/rtflc/instructions/ClauseOpenerInstruction.java
+++
@@ -1,8 +0,0 @@
-package net.termer.rtflc.instructions;
-
-/**
- * Identifier instruction to distinguish clause opener instructions with others
- * @author termer
- * @since 1.0
- */
-public interface ClauseOpenerInstruction extends RtflInstruction {}
--- src/net/termer/rtflc/type/NumberType.java
+++
@@ -1,6 +0,0 @@
-package net.termer.rtflc.type;
-
-public interface NumberType extends RtflType {
-	public int toInt();
-	public double toDouble();
-}
